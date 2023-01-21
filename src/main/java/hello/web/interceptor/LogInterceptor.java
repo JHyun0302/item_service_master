32be1733f721fbb40f237e72f6dfcdd265e456aa
jaehyun <wogustkfkd11@gmail.com>
2023-01-21 23:11:51 +0900

    스프링 인터셉터 방법

---
+++ src/main/java/hello/web/interceptor/LogInterceptor.java
@@ -0,0 +1,62 @@
+package hello.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+/**
+ * 스프링 인터셉터(스프링 MVC 이용)
+ */
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    /**
+     * 컨트롤러 호출 전에 호출됨
+     */
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOG_ID, uuid);
+        //서블릿 필터의 경우 지역변수로 담아둠! But 스프링 인터셉터의 경우 에러 난 경우 postHandle이 호출 X, 따라서 request에 담아둠!
+
+        //@RequestMapping: HandlerMethod
+        //정적 리소스: ResourceHttpRequestHandler
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler; //호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+            //hm.get() 으로 정보들 꺼내 쓸 수 있음!!
+        }
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+        return true; //false 진행X
+    }
+
+    /**
+     * 컨트롤러 호출 후에 호출됨(예외 발생시 postHandle 호출 X)
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandler [{}]", modelAndView);
+    }
+
+    /**
+     * 뷰가 렌더링 된 이후에 호출됨(예외 발생해도 항상 호출 O - 공통 처리)
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = (String) request.getAttribute(LOG_ID); //공통 처리(preHandle: setAttribute)
+        log.info("RESPONSE [{}][{}][{}]", uuid, requestURI, handler);
+        if (ex != null) {
+            log.info("afterCompletion error!!", ex);
+        }
+    }
+}
