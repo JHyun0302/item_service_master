6c26a8a7bb32f0d7bc9e1f533998377ffcb0b254
jaehyun <wogustkfkd11@gmail.com>
2023-01-22 19:09:27 +0900

    ArgumentResolver: 로그인 회원 편리하게 찾기

---
+++ src/main/java/hello/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,45 @@
+package hello.web.argumentresolver;
+
+import hello.domain.member.Member;
+import hello.web.SessionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 로그인 회원을 편리하게 찾기 (ArgumentResolver 사용)
+ */
+@Slf4j
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        log.info("supportParameter 실행");
+
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        //직접 만든 @Login가 파라미터에 붙어있는지?
+        boolean hasMemberType = Member.class.isAssignableFrom(parameter.getParameterType());
+        //.getParameterType() == @Login Member loginMember에 hashMemberType: Member 타입이냐?
+
+        return hasLoginAnnotation && hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        log.info("resolverArgument 실행");
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false); //true: 의미없는 세션 생성 - 메모리 낭비
+        if (session == null) {
+            return null;
+        }
+
+        return session.getAttribute(SessionConst.LOGIN_MEMBER);
+    }
+}
