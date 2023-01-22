4cfcc3e01b4d95999b258000ceda6cc1a061f182
jaehyun <wogustkfkd11@gmail.com>
2023-01-22 19:08:57 +0900

    ArgumentResolver: 로그인 회원 편리하게 찾기

--- src/main/java/hello/web/HomeController.java
+++ src/main/java/hello/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.web;
 
 import hello.domain.member.Member;
 import hello.domain.member.MemberRepository;
+import hello.web.argumentresolver.Login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -15,13 +16,25 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 public class HomeController {
     private final MemberRepository memberRepository;
 
-    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginSpring( //required = false -> 로그인 안 한 사용자도 접속 가능
                                    @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
         if (loginMember == null) {
             return "home";
         }
 
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginArgumentResolver(@Login Member loginMember, Model model) {
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
