97ec326c35d5fd4c49dada07bab2858e23965117
jaehyun <wogustkfkd11@gmail.com>
2023-01-23 23:08:27 +0900

    체크박스 수정 폼 저장

--- src/main/java/hello/web/item/form/ItemUpdateForm.java
+++ src/main/java/hello/web/item/form/ItemUpdateForm.java
@@ -1,9 +1,11 @@
 package hello.web.item.form;
 
+import hello.domain.item.itemService.ItemType;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Data
 public class ItemUpdateForm {
@@ -18,4 +20,9 @@ public class ItemUpdateForm {
     private Integer price;
 
     private Integer quantity;
+
+    private Boolean open; //판매 여부
+    private List<String> regions; //등록 지역
+    private ItemType itemType;//상품 종류
+    private String deliveryCode;//배송 방식
 }
