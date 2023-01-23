7fb723022472140cdb9cf456ba08f843e1c5bc29
jaehyun <wogustkfkd11@gmail.com>
2023-01-23 23:07:10 +0900

    체크박스 내용 저장

--- src/main/java/hello/web/item/ItemController.java
+++ src/main/java/hello/web/item/ItemController.java
@@ -1,7 +1,9 @@
 package hello.web.item;
 
-import hello.domain.item.Item;
-import hello.domain.item.ItemRepository;
+import hello.domain.item.itemService.DeliveryCode;
+import hello.domain.item.itemService.Item;
+import hello.domain.item.itemService.ItemRepository;
+import hello.domain.item.itemService.ItemType;
 import hello.web.item.form.ItemSaveForm;
 import hello.web.item.form.ItemUpdateForm;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/items")
@@ -21,6 +26,36 @@ import java.util.List;
 @Slf4j
 public class ItemController {
     private final ItemRepository itemRepository;
+
+    /**
+     * 상품 add, edit, item에 모두  Map<String, String> regions = new LinkedHashMap<>(); 해야함! (코드 중복!)
+     *
+     * @Controller 시작할 때 자동으로 model에 addAttribute됨
+     */
+
+    @ModelAttribute("regions") //자바 코드
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>(); //hashMap은 순서 보장이 안됨!
+        regions.put("SEOUL", "서울"); //"SEOUL": 시스템상 key, "서울": 사용자에게 보이는 값
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions; //model.addattribute("regions", regions) 자동으로 모델에 담김
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values(); //enum의 모든 정보를 배열로 반환: [BOOK, FOOD, ETC]
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -62,6 +97,10 @@ public class ItemController {
         item.setItemName(form.getItemName());
         item.setPrice(form.getPrice());
         item.setQuantity(form.getQuantity());
+        item.setOpen(form.getOpen());
+        item.setRegions(form.getRegions());
+        item.setItemType(form.getItemType());
+        item.setDeliveryCode(form.getDeliveryCode());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -77,7 +116,7 @@ public class ItemController {
     }
 
     @PostMapping("{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute("item") ItemUpdateForm form, BindingResult bindingResult) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute("editItem") ItemUpdateForm form, BindingResult bindingResult) {
         //특정 필드가 아닌 복합 룰 검증
         if (form.getPrice() != null && form.getQuantity() != null) {
             int resultPrice = form.getPrice() * form.getQuantity();
@@ -94,6 +133,10 @@ public class ItemController {
         itemParam.setItemName(form.getItemName());
         itemParam.setQuantity(form.getQuantity());
         itemParam.setPrice(form.getPrice());
+        itemParam.setOpen(form.getOpen());
+        itemParam.setRegions(form.getRegions());
+        itemParam.setItemType(form.getItemType());
+        itemParam.setDeliveryCode(form.getDeliveryCode());
 
         itemRepository.update(itemId, itemParam);
         return "redirect:/items/{itemId}";
